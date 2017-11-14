b0e44bab7c9dde3ceecdabb62739fd39ad1f0552
klx3300 <inc19991@gmail.com>
2017-11-15 00:14:48 +0800

    lab01 finished

---
+++ lab1/linear_list.c
@@ -0,0 +1,174 @@
+#include "linear_list_interface.h"
+#include <stdlib.h>
+#include <string.h>
+/*
+ * @Name InitaList
+ * @Args *l is the linear list being operated
+ * @Return Success or Failure
+ */
+ int InitaList(SqList * l){
+     memset(l,0,sizeof(SqList));
+    return SUCCESS;
+ }
+ 
+ /*
+  * @Name DestroyList
+  * @Args *l is the linear list being operated
+  * @Return Success or Failure
+  */
+ int DestroyList(SqList * l){
+     if(l->elem!=NULL){
+         free(l->elem);
+     }
+     return InitaList(l);
+ }
+ 
+ /*
+  * @Name ClearList
+  * @Args *l is the linear list being operated
+  * @Return Success or Failure
+  */
+ int ClearList(SqList * l){
+     memset(l->elem,0,l->listsize);
+     l->length=0;
+     return SUCCESS;
+ }
+ 
+ /*
+  * @Name ListEmpty
+  * @Args *l is the linear list being operated
+  * @Return True or False
+  */
+ int ListEmpty(SqList * l){
+     return l->length==0;
+ }
+ 
+ /*
+  * @Name ListLength
+  * @Args *l is the linear list being operated
+  * @Return an integer which is the length of List
+  */
+ int ListLength(SqList * l){
+     return l->length;
+ }
+ 
+ /*
+  * @Name GetElem
+  * @Args *l is the linear list being operated
+  *        i is the position of elem
+  *       *e is the elem
+  * @Return Success or Failure
+  */
+ int GetElem(SqList * l, int i, int * e){
+     if(i >= l->length) return FAILURE;
+     *e=(l->elem[i]);
+     return SUCCESS;
+ }
+ 
+ /*
+  * @Name LocateElem
+  * @Args *l is the linear list being operated
+  *        e is the value provided
+  *       compare(a, b) is the function which is provided below
+  * @Return an integer which is the position of elem who is able to pass the compare function
+  *      and 0 is expected when no one can pass
+  */
+ int LocateElem(SqList * l, int e, int (* compare) (int a, int b)){
+     for(int i=0;i<l->length;i++){
+         if(compare(l->elem[i],e)) return i;
+     }
+     return 0;
+ }
+ 
+ /*
+  * @Name PriorElem
+  * @Args *l is the linear list being operated
+  *        cur_e is the possible elem
+  *       *pre_e is the pre-elem
+  * @Return Success or Failure
+  */
+ int PriorElem(SqList * l, int cur_e, int * pre_e){
+     for(int i=0;i<l->length;i++){
+         if(l->elem[i]==cur_e){
+             if(i==0) return FAILURE;
+             *pre_e =  l->elem[i-1];
+             return SUCCESS;
+         }
+     }
+     return FAILURE;
+ }
+ 
+ /*
+  * @Name NextElem
+  * @Args *l is the linear list being operated
+  *        cur_e is the possible elem
+  *       *next_e is the next-elem
+  * @Return Success or Failure
+  */
+ int NextElem(SqList * l, int cur_e, int * next_e){
+     for(int i=0;i<l->length;i++){
+         if(l->elem[i]==cur_e){
+             if(i==l->length-1) return FAILURE;
+             *next_e =  l->elem[i+1];
+             return SUCCESS;
+         }
+     }
+     return FAILURE;
+ }
+ 
+ /*
+  * @Name ListInsert
+  * @Args *l is the linear list being operated
+  *        i is the aim position
+  *        e is the elem
+  * @Return Success or Failure
+  */
+ int ListInsert(SqList * l, int pos, int e){
+     if(pos>l->length) return FAILURE;
+     if(l->listsize < l->length+1){
+        int* tmprealloc = malloc(sizeof(int)*(l->listsize)*2);
+        if(tmprealloc == NULL) return FAILURE;
+        l->listsize*=2;
+        memset(tmprealloc,0,sizeof(int)*(l->listsize));
+        memcpy(tmprealloc,l->elem,l->length*sizeof(int));
+        l->elem = tmprealloc;
+     }
+     for(int i=l->length;i>pos;i--){
+         l->elem[i]=l->elem[i-1];
+     }
+     l->elem[pos]=e;
+     l->length++;
+     return SUCCESS;
+ }
+ 
+ /*
+  * @Name ListDelete
+  * @Args *l is the linear list being operated
+  *        i is the aim position
+  *       *e is the elem
+  * @Return Success or Failure
+  */
+ int ListDelete(SqList * l, int pos, int * e){
+     if(pos>l->length-1) return FAILURE;
+     *e = l->elem[pos];
+     for(int i=pos;i<l->length-1;i++){
+         l->elem[pos] = l->elem[pos+1];
+     }
+     l->length--;
+     return SUCCESS;
+ }
+ 
+ /*
+  * @Name ListTraverse
+  * @Args *l is the linear list being operated
+  *       visit(e) is the function which is provided below
+  * @Return Success or Failure
+  */
+ int ListTraverse(SqList * l, int (* visit) (int e)){
+     if(ListEmpty(l)) return FAILURE;
+     for(int i=0;i<l->length;i++){
+         visit(l->elem[i]);
+     }
+     return SUCCESS;
+ }
+ 
---
+++ lab1/linear_list_interface.h
@@ -0,0 +1,137 @@
+#ifndef HUSTDS_LINEAR_LIST_INTERFACE
+#define HUSTDS_LINEAR_LIST_INTERFACE
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define SUCCESS 1
+#define FAILURE 0
+
+typedef struct{
+    int * elem;    // pointer of the start position of elems list
+    int length;    // current length of list
+    int listsize;  // size of memory which has been malloc for list
+}SqList;
+
+/*
+ * @Name InitaList
+ * @Args *l is the linear list being operated
+ * @Return Success or Failure
+ */
+int InitaList(SqList * l);
+
+/*
+ * @Name DestroyList
+ * @Args *l is the linear list being operated
+ * @Return Success or Failure
+ */
+int DestroyList(SqList * l);
+
+/*
+ * @Name ClearList
+ * @Args *l is the linear list being operated
+ * @Return Success or Failure
+ */
+int ClearList(SqList * l);
+
+/*
+ * @Name ListEmpty
+ * @Args *l is the linear list being operated
+ * @Return True or False
+ */
+int ListEmpty(SqList * l);
+
+/*
+ * @Name ListLength
+ * @Args *l is the linear list being operated
+ * @Return an integer which is the length of List
+ */
+int ListLength(SqList * l);
+
+/*
+ * @Name GetElem
+ * @Args *l is the linear list being operated
+ *        i is the position of elem
+ *       *e is the elem
+ * @Return Success or Failure
+ */
+int GetElem(SqList * l, int i, int * e);
+
+/*
+ * @Name LocateElem
+ * @Args *l is the linear list being operated
+ *        e is the value provided
+ *       compare(a, b) is the function which is provided below
+ * @Return an integer which is the position of elem who is able to pass the compare function
+ *      and 0 is expected when no one can pass
+ */
+int LocateElem(SqList * l, int e, int (* compare) (int a, int b));
+
+/*
+ * @Name PriorElem
+ * @Args *l is the linear list being operated
+ *        cur_e is the possible elem
+ *       *pre_e is the pre-elem
+ * @Return Success or Failure
+ */
+int PriorElem(SqList * l, int cur_e, int * pre_e);
+
+/*
+ * @Name NextElem
+ * @Args *l is the linear list being operated
+ *        cur_e is the possible elem
+ *       *next_e is the next-elem
+ * @Return Success or Failure
+ */
+int NextElem(SqList * l, int cur_e, int * next_e);
+
+/*
+ * @Name ListInsert
+ * @Args *l is the linear list being operated
+ *        i is the aim position
+ *        e is the elem
+ * @Return Success or Failure
+ */
+int ListInsert(SqList * l, int i, int e);
+
+/*
+ * @Name ListDelete
+ * @Args *l is the linear list being operated
+ *        i is the aim position
+ *       *e is the elem
+ * @Return Success or Failure
+ */
+int ListDelete(SqList * l, int i, int * e);
+
+/*
+ * @Name ListTraverse
+ * @Args *l is the linear list being operated
+ *       visit(e) is the function which is provided below
+ * @Return Success or Failure
+ */
+int ListTraverse(SqList * l, int (* visit) (int e));
+
+/*
+ * @Name compare
+ * @Args a is the first var
+ *       b is the second var
+ * @Return True or False
+ * @Usage this function is expected to be called in LocateElem
+ */
+int compare(int a, int b) {
+    return a == b ? TRUE : FALSE;
+}
+
+/*
+ * @Name visit
+ * @Args e
+ * @Return void
+ * @Usage this function is expected to be called in ListTraverse
+ */
+void visit(int e) {
+    printf("%d ", e);
+}
+
+#endif
